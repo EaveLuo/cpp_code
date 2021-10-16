371c6d7e4cbb1925fcaaf944a645c26f3d5e6cbc
Eave Luo <846899026@qq.com>
2021-10-16 16:48:41 +0800

    over the 1013 home works

--- c_learning/1013/test11.cpp
+++ c_learning/1013/test11.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    
+#define numbersSize 4
+
+// 此处排序可以采用经典的冒泡排序算法
+void bubbleSort(int inputNumbers[])
+{
+    for (int i = 0; i < numbersSize - 1; i++)
+    {
+        bool flag = false;
+        for (int j = numbersSize - 1; j > i; j--)
+        {
+            if (inputNumbers[j - 1] > inputNumbers[j])
+            {
+                int temp = inputNumbers[j - 1];
+                inputNumbers[j - 1] = inputNumbers[j];
+                inputNumbers[j] = temp;
+                flag = true;
+            }
+        }
+        if (flag == false)
+            return;
+    }
+}
+
+int main()
+{
+    int inputNumbers[numbersSize];
+    for (int i = 0; i < numbersSize; i++)
+    {
+        cout << "please enter the " << i + 1 << " number:";
+        cin >> inputNumbers[i];
+    }
+    bubbleSort(inputNumbers);
+    // output the right sort
+    cout << "the right sort numbers is:";
+    for (int i = 0; i < numbersSize; i++)
+    {
+        cout << inputNumbers[i] << ",";
+    }
+    cout << endl;
     return 0;
 }
