a72f453e7b9d9861b07f163116aa883ae07c4ed7
Eave Luo <846899026@qq.com>
2021-11-03 09:33:27 +0800

    add 1103 c classwork

---
+++ c_learning/classWork/1103NineMulNine.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 9; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << "*" << i << "=" << i * j << "\t";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
